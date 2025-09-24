1de7635ef5cb6eac3df3026e9ec6e6ea6c1b3ea9
grz3chotnik <krzysztofkubinski2004@gmail.com>
2025-09-24 15:42:11 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import {MemoryRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+// @ts-ignore
 import React, {useRef, useState, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import SpotiPlayer from './components/SpotiPlayer.js';
+import {Analytics} from "@vercel/analytics/next"
 
 export default function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<SpotiPlayer/>}/>
-          <Route path="/spotiplayer" element={<SpotiPlayer/>}/>
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+
+
+        <>
+            <Router>
+                <div>
+
+                    <Routes>
+                        <Route path="/" element={<SpotiPlayer/>}/>
+                        <Route path="/spotiplayer" element={<SpotiPlayer/>}/>
+                    </Routes>
+                </div>
+            </Router>
+        </>
+
+    );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { Analytics } from '@vercel/analytics/react'; // Add this import
 
 createRoot(document.getElementById('root')).render(
-    <App />
-)
+    <>
+        <Analytics />
+        <App />
+    </>
+);
