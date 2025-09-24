4ac5a59f1f306c7735f9ac265d66af800b8ed51b
grz3chotnik <krzysztofkubinski2004@gmail.com>
2025-09-24 16:14:00 -0300

    select fix

--- src/components/SpotiPlayer.tsx
+++ src/components/SpotiPlayer.tsx
@@ -13,6 +13,7 @@ const spotifyApi = new SpotifyWebApi();
 
 const CLIENT_ID = 'cff99ec39a2c4666bfaeaf792e4aaa7b';
 const REDIRECT_URI = 'https://ipod.2004.lol/';
+// const REDIRECT_URI = 'http://localhost:1212/';
 
 const SCOPES = [
     'playlist-read-private',
