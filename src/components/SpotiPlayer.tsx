e95e134f8793623fc40f97615f0bda107e6092c4
grz3chotnik <113199837+grz3chotnik@users.noreply.github.com>
2025-11-28 13:17:42 -0300

    Fix REDIRECT_URI trailing slash

--- src/components/SpotiPlayer.tsx
+++ src/components/SpotiPlayer.tsx
@@ -15,7 +15,7 @@ import styles from '../index.module.css';
 const spotifyApi = new SpotifyWebApi();
 
 const CLIENT_ID = 'cff99ec39a2c4666bfaeaf792e4aaa7b';
-const REDIRECT_URI = 'https://ipod.2004.lol';
+const REDIRECT_URI = 'https://ipod.2004.lol/';
 // const REDIRECT_URI = 'http://localhost:1212/';
 // const REDIRECT_URI = 'http://192.168.1.53:1212/';
 
